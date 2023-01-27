323ca3de1a7aeb5fcffec1460fffd5cdc4fd85b1
Developer <pablogarcirodriguez@gmail.com>
2023-01-27 12:15:10 +0100

    Clase 11: Navegación entre rutas

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <div>404 NOT FOUND</div>
+      <img src="https://cdn-icons-png.flaticon.com/512/1401/1401101.png" />
+    </div>
+  );
+};
+
+export default NotFound;
